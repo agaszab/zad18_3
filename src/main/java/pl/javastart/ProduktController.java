67186b056ec547b430dd31055ad4532229a8b41e
agaszab <gatito@poczta.pl>
2018-03-07 21:31:57 +0100

    produkty na www

--- src/main/java/pl/javastart/ProduktController.java
+++ src/main/java/pl/javastart/ProduktController.java
@@ -3,8 +3,10 @@ package pl.javastart;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/")
@@ -21,26 +23,47 @@ public class ProduktController {
 
     @RequestMapping("/list")
     @ResponseBody
+    public String wyswietlProdukty(HttpServletRequest request) {
+
+      String kat = request.getParameter("kategorie");
+
+        if (kat==null || kat.equals("")) { return   wyswietlProdukty(); }
+        if (kat.equals("DOMOWE")) { return   wyswietl(Kategorie.DOMOWE);  }
+        if (kat.equals("INNE")) { return   wyswietl(Kategorie.INNE);  }
+        if (kat.equals("SPOZYWCZE")) { return   wyswietl(Kategorie.SPOZYWCZE);  }
+        return "nic";
+    }
+
+
+  //  @RequestMapping("/list")
+    @ResponseBody
     public String wyswietlProdukty() {
 
-        // List<Produkt> produkty=listaProduktow.pobierzProdukty();
+        List<Produkt> produkty=listaProduktow.pobierzProdukty();
 
         String result = "";
 
-        for (Produkt pr : listaProduktow.pobierzProdukty()) {
-            result += pr;
+        for (Produkt pr : produkty) {
+            result += pr.toString();
+        }
+       return result;
+
+    }
+
+    public String wyswietl (Kategorie kategorie){
+        List<Produkt> produkty=listaProduktow.pobierzProdukty();
+
+        String result = "";
+
+        for (Produkt pr : produkty) {
+            if (pr.getKategoria()==kategorie)
+            result += pr.toString();
         }
         return result;
+
     }
 
 
-    @ResponseBody
-    @RequestMapping("/list1")
-    public String start(HttpServletRequest request) {
 
-        String kat = request.getParameter("kategorie");
-        if (kat.equals("")) { return "produkty.html"; }
 
-            return "";
-    }
 }
